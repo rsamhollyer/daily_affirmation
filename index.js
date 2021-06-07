0049dc769b981a303cca13f1f188a62ed624f470
Sam Hollyer <rsamhollyer@gmail.com>
2021-06-07 10:04:27 -0500

    This is a lazy push

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const { App } = require('@slack/bolt');
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: process.env.APP_TOKEN,
 });
 
 app.command('/affirm', async ({ command, ack, say }) => {
